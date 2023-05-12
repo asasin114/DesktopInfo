d9f47450dfa6208c67e6cff580615b21cfb3e280
Taylor Bornyk <taylor@insighthosting.com>
2023-05-12 16:53:32 -0600

    Update to Initial Setup

--- install.ps1
+++ install.ps1
@@ -19,7 +19,7 @@ taskkill /IM DesktopInfo64.exe /F
 New-item -itemtype directory -force -path "$Path_4netIntune\Program\DesktopInfo"
 Invoke-WebRequest -Uri "$url/DesktopInfo64.exe" -OutFile "$output\DesktopInfo64.exe"
 Invoke-WebRequest -Uri "$url/hostname.ini" -OutFile "$output\hostname.ini"
-Invoke-WebRequest -Uri "$url/DesktopInfo64.ps1" -OutFile "$output\DesktopInfo.ps1"
+Invoke-WebRequest -Uri "$url/DesktopInfo.ps1" -OutFile "$output\DesktopInfo.ps1"
 
 ###########################################################################################
 # Create dummy vbscript to hide PowerShell Window popping up at logon
